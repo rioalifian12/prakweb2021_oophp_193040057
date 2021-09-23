dc34692e762c1cae9e452ae0ebecaef45db59f46
rioalifian12 <rioalifian12@gmail.com>
2021-09-23 13:52:43 +0700

    Telah menyelesaikan video Constant

---
+++ constant.php
@@ -0,0 +1,32 @@
+<?php
+
+// define('NAMA', 'Sandhika Galih');
+// echo NAMA;
+// echo "<br>";
+
+// const UMUR = 32;
+// echo UMUR;
+
+// class Coba
+// {
+//   const NAMA = 'Sandhika Galih';
+// }
+
+// echo Coba::NAMA;
+
+// echo __FILE__;
+
+// function coba()
+// {
+//   return __FUNCTION__;
+// }
+
+// echo coba();
+
+class Coba
+{
+  public $kelas = __CLASS__;
+}
+
+$obj = new Coba;
+echo $obj->kelas;
